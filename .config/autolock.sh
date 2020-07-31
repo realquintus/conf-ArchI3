214c41fb5c2ca3235569a3dca0ee21aeae188a77
realquintus <pablohousse@gmail.com>
2020-07-31 18:08:40 +0000

    Adding xorg conf and create dir

--- .config/autolock.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-i3-nagbar -t warning -m 'You have been afk for too long, do something or it will sadly lock... 10 seconds left... Tic Tac...' &
-sleep 10
-killall i3-nagbar
-i3-nagbar -t warning -m 'Were you asleep?' &
-dm-tool lock
--- .config/picom.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# If picom is running, kill it to prevent multiple instances
-if ps -A | grep picom; then
-	killall -q picom
-fi
-
-# Load picom
-picom --config /home/pablo/.config/picom.conf
